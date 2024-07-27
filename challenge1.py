e9a68557c5a47eddcc8ee851e9ae49734693dd01
Tnr1112 <tnr1112@hotmail.com>
2024-07-26 21:25:01 -0300

    Create a function that returns the board with the number of adjacent mines in each cell

--- challenge1.py
+++ challenge1.py
@@ -6,6 +6,12 @@ def main():
 		[1, 1, 0, 0]
 	]
 
+	boardWithMinesCount = minesCountBoard(board)
+	print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+                  for row in board]), end='\n--------------------\n')
+	print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+                  for row in boardWithMinesCount]))
+
 def getMinesIndexes(board):
 	minesIndexes = []
 	for i in range(len(board)):
@@ -13,10 +19,27 @@ def getMinesIndexes(board):
 			if board[i][j] == 1:
 				minesIndexes.append((i, j))
 
+	return minesIndexes
+
 def copyMatrixAndFillZero(board):
 	rows = len(board)
 	cols = len(board[0] if rows > 0 else 0)
 	return [[0 for _ in range(cols)] for _ in range(rows)]
+
+def minesCountBoard(board):
+	minesIndexes = getMinesIndexes(board)
+	newBoard = copyMatrixAndFillZero(board)
+
+	for mine in minesIndexes:
+		x, y = mine
+		newBoard[x][y] = 9
+		for i in range(x-1, x+2):
+			for j in range(y-1, y+2):
+				if 0 <= i < len(board) and 0 <= j < len(board[i]):
+					if newBoard[i][j] != 9:
+						newBoard[i][j] += 1
+
+	return newBoard
 
 if __name__ == "__main__":
 	main()
