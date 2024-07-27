e2ca7b29c4425707b23f0f4743653b12d02c79b9
Tnr1112 <tnr1112@hotmail.com>
2024-07-26 22:05:30 -0300

    Refactor the code to make it more efficient and simplified

--- challenge1.py
+++ challenge1.py
@@ -1,3 +1,27 @@
+MINE = 9
+
+def minesCountBoard(board):
+	rows, cols = len(board), len(board[0])
+	newBoard = [[0 for _ in range(cols)] for _ in range(rows)]
+
+	for i in range(rows):
+		for j in range(cols):
+			if board[i][j] == 1:
+				newBoard[i][j] = MINE
+				mineIndex = (i, j)
+				updateNeighbours(newBoard, mineIndex)
+
+	return newBoard
+
+def updateNeighbours(board, mineIndex):
+	rows = len(board)
+	cols = len(board[0] if rows > 0 else 0)
+	x, y = mineIndex
+	for i in range(x-1, x+2):
+		for j in range(y-1, y+2):
+			if 0 <= i < rows and 0 <= j < cols and board[i][j] != MINE:
+					board[i][j] += 1
+
 def main():
 	board = [
 		[0, 1, 0, 0],
@@ -7,39 +31,12 @@ def main():
 	]
 
 	boardWithMinesCount = minesCountBoard(board)
+	print('Input board:')
 	print('\n'.join([''.join(['{:4}'.format(item) for item in row])
                   for row in board]), end='\n--------------------\n')
+	print('Mines count board:')
 	print('\n'.join([''.join(['{:4}'.format(item) for item in row])
                   for row in boardWithMinesCount]))
-
-def getMinesIndexes(board):
-	minesIndexes = []
-	for i in range(len(board)):
-		for j in range(len(board[i])):
-			if board[i][j] == 1:
-				minesIndexes.append((i, j))
-
-	return minesIndexes
-
-def copyMatrixAndFillZero(board):
-	rows = len(board)
-	cols = len(board[0] if rows > 0 else 0)
-	return [[0 for _ in range(cols)] for _ in range(rows)]
-
-def minesCountBoard(board):
-	minesIndexes = getMinesIndexes(board)
-	newBoard = copyMatrixAndFillZero(board)
-
-	for mine in minesIndexes:
-		x, y = mine
-		newBoard[x][y] = 9
-		for i in range(x-1, x+2):
-			for j in range(y-1, y+2):
-				if 0 <= i < len(board) and 0 <= j < len(board[i]):
-					if newBoard[i][j] != 9:
-						newBoard[i][j] += 1
-
-	return newBoard
 
 if __name__ == "__main__":
 	main()
